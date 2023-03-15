927904d2fd66058a458f197e648c2c71f0864bce
Hico <70553434+HicoMcD@users.noreply.github.com>
2023-03-15 12:57:49 +0200

    5 tests passing

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ describe('Escrow', function () {
   let beneficiary;
   let arbiter;
   let amount = ethers.utils.parseEther('1');
+  let newAmount = ethers.utils.parseEther('5');
   // const deposit = ethers.utils.parseEther('1');
   beforeEach(async () => {
     depositor = ethers.provider.getSigner(0);
@@ -32,6 +33,7 @@ describe('Escrow', function () {
   describe('after approval from address other than the arbiter', () => {
     it('should revert', async () => {
       await expect(contract.connect(beneficiary).approve()).to.be.reverted;
+      await expect(contract.connect(depositor).approve()).to.be.reverted;
     });
   });
 
@@ -44,4 +46,25 @@ describe('Escrow', function () {
       expect(after.sub(before)).to.eq(amount);
     });
   });
+
+  describe('after funds sent to depositor, can change amount now and approve for beneficiary', () => {
+    it('should let the depositor change the depsosit amount', async () => {
+      const approveTxn = await contract.connect(arbiter).approve();
+      await approveTxn.wait();
+
+      const newDepositAmountTxn = await contract.connect(depositor).newDepositAmount(newAmount, {value: newAmount});
+      await newDepositAmountTxn.wait();
+
+      const newContractBalance = await ethers.provider.getBalance(contract.address);
+      expect(newContractBalance).to.eq(newAmount);
+      // console.log(newContractBalance);
+      const before = await ethers.provider.getBalance(beneficiary.getAddress());
+      const approveTxn2 = await contract.connect(arbiter).approve();
+      await approveTxn2.wait();
+      const after = await ethers.provider.getBalance(beneficiary.getAddress());
+      expect(after.sub(before).add(amount)).to.eq(newAmount.add(amount));
+    })
+  })
+
 });
+
